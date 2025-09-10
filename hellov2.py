f735e2de9a9bf765379a1a06b5595f8d76998b05
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2025-09-10 10:34:15 +0200

    Types

--- hellov2.py
+++ hellov2.py
@@ -1,3 +1,16 @@
+#First name
 first_name=input("Enter your firstname! ")
+#Last Name
 last_name=input("Enter your lastname! ")
-print(f"Hello {first_name} {last_name}!")
+#Age
+age=int(input("Enter your age!"))
+
+height=float(input("Enter your height!"))
+
+#Final Greeting
+print(f"Hello {first_name} {last_name}! Your age is {age}!")
+
+print(type(first_name))
+print(type(last_name))
+print(age + 51)
+print(f'{(height + 0.1):.2f}')
---
+++ print_type.py
@@ -0,0 +1,11 @@
+name = "Daniel Pimbor"
+age = 16
+height = 1.84
+is_student = True
+
+print(f"Name : {name} \nAge: {age} \nHeight: {height} \nIs student? {is_student}")
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
