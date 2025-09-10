059f1093aa5beaef577fcdd2db5fb741d85fc6a3
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2025-09-10 11:25:31 +0200

    3 db feladat

---
+++ Feladatok3/3_1.py
@@ -0,0 +1,33 @@
+"""1. Feladat
+Thonny fejlesztői környezetben készíts egy rövid programot, amely
+
+    kommentként tartalmazza a program funkciójának leírását,
+    konstansként tárolja PI értékét,
+    egy másik változóban tárolja egy kör sugarának nagyságát (cm-ben megadva),
+    kiszámolja és a képernyőre kiírja a kör kerületét és területét!
+
+[Megjegyzés] A szorzás jele: *"""
+
+
+
+PI = 3.14
+radius = int(input("Add meg a kör sugarát: "))
+kerulet = PI * 2 * radius
+terulet = PI * radius * radius
+
+print(f"Kerület: {kerulet} cm")
+print(f"Terület: {terulet} cm²")
+
+while True:
+    valasz = input("Szeretnél kerekíteni? (Igen/Nem): ").strip().lower()
+    
+    if valasz == "igen":
+        print(f"Kerekített kerület: {int(kerulet)} cm")
+        print(f"Kerekített terület: {int(terulet)} cm²")
+        print("A művelet elvégezve.")
+        break
+    elif valasz == "nem":
+        print("A művelet elvégezve.")
+        break
+    else:
+        print("Érvénytelen válasz. Kérlek, írd be: Igen vagy Nem.")
---
+++ Feladatok3/3_2.py
@@ -0,0 +1,9 @@
+"""Készíts egy rövid programot, amely egy változóban eltárol egy szót. Próbáld meg ennek a változónak a tartalmát int értékké átalakítani. 
+Mit tapasztalsz? Milyen üzenet jelenik meg a képernyőn?
+"""
+
+szo = "ember"
+szam = int(szo)
+print(szam)
+
+#ValueError: invalid literal for int() with base 10: 'ember'
